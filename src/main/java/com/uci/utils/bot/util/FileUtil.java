b81c36649c0aaa572c60dd987f22936c8b6ebcc4
Surabhi <surabhi.mahawar@aurigait.com>
2022-11-09 18:40:57 +0530

    file upload changes

--- src/main/java/com/uci/utils/bot/util/FileUtil.java
+++ src/main/java/com/uci/utils/bot/util/FileUtil.java
@@ -1,15 +1,82 @@
 package com.uci.utils.bot.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.MimeTypeUtils;
 
+@Slf4j
 public class FileUtil {
+	/**
+	 * Get file local path by file url
+	 * @param urlStr
+	 * @param mimeType
+	 * @param name
+	 * @param maxSizeForMedia
+	 * @return
+	 */
+	public static String downloadFileToLocalFromUrl(String urlStr, String mimeType, String name, Double maxSizeForMedia) {
+		/* Unique File Name */
+		name = getUploadedFileName(mimeType, name);
+
+		/* File input stream to copy from */
+		try {
+			URL url = new URL(urlStr);
+			byte[] inputBytes = url.openStream().readAllBytes();
+
+			/* Discard if file size is greater than MAX_SIZE_FOR_MEDIA */
+			if (maxSizeForMedia != null && inputBytes.length > maxSizeForMedia) {
+				log.error("file size is greater than limit : " + inputBytes.length);
+				return "";
+			}
+
+			/* Create temp file to copy to */
+			String localPath = "/tmp/";
+			String filePath = localPath + name;
+			File temp = new File(filePath);
+			temp.createNewFile();
+
+			// Copy file from url to temp file
+			Files.copy(new ByteArrayInputStream(inputBytes), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+
+			return filePath;
+		} catch (MalformedURLException e) {
+			log.error("MalformedURLException in getFilePathFromUrl : "+e.getMessage());
+		} catch (IOException e) {
+			log.error("IOException in getFilePathFromUrl : "+e.getMessage());
+		}
+
+		return "";
+	}
+
+	/**
+	 * Get file name from mimeType and name
+	 * @param mimeType
+	 * @param name
+	 * @return
+	 */
+	public static String getUploadedFileName(String mimeType, String name) {
+		String ext = com.uci.utils.bot.util.FileUtil.getFileTypeByMimeSubTypeString(MimeTypeUtils.parseMimeType(mimeType).getSubtype());
+
+		if (name == null || name.isEmpty()) {
+			name = UUID.randomUUID().toString();
+		}
+		name += "." + ext;
+
+		return name;
+	}
 
 	/**
 	 * Function to get Mime type of file from url
