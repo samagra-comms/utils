5b0da0ac89da36262bf916a6c445367d2ff62189
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-05-15 02:07:07 +0530

    Added logs in BotService for cache and uci-api response

--- src/main/java/com/uci/utils/BotService.java
+++ src/main/java/com/uci/utils/BotService.java
@@ -63,7 +63,7 @@ public class BotService {
 								.build())
 						.retrieve().bodyToMono(String.class).map(response -> {
 							if (response != null) {
-								log.info(response);
+								log.info("Call getBotNodeFromStartingMessage : " + response + " cache : " + cache.getIfPresent(cacheKey));
 								try {
 									ObjectMapper mapper = new ObjectMapper();
 									JsonNode root = mapper.readTree(response);
@@ -106,7 +106,7 @@ public class BotService {
 												.build())
 						.retrieve().bodyToMono(String.class).map(response -> {
 							if (response != null) {
-								log.info(response);
+								log.info("Call getBotNodeFromName : " + response + " cache : " + cache.getIfPresent(cacheKey));
 								try {
 									ObjectMapper mapper = new ObjectMapper();
 									JsonNode root = mapper.readTree(response);
@@ -146,6 +146,7 @@ public class BotService {
 						.bodyToMono(String.class)
 						.map(response -> {
 									if (response != null) {
+										log.info("Call getBotNodeFromId : " + response + " cache : " + cache.getIfPresent(cacheKey));
 										ObjectMapper mapper = new ObjectMapper();
 										try {
 											JsonNode root = mapper.readTree(response);
@@ -165,48 +166,49 @@ public class BotService {
 				.log("cache");
 	}
 
-	/**
-	 * Get Adapter id by bot name
-	 * @param botName
-	 * @return
-	 */
-	public Mono<String> getAdapterIdFromBotName(String botName) {
-		String cacheKey = "valid-adpater-from-bot-name: " + botName;
-		return CacheMono.lookup(key -> Mono.justOrEmpty(cache.getIfPresent(cacheKey) != null ? cache.getIfPresent(key).toString() : null)
-					.map(Signal::next), cacheKey)
-				.onCacheMissResume(() -> webClient.get()
-						.uri(builder -> builder.path("admin/bot/search/internal")
-								.queryParam("perPage", 5)
-								.queryParam("page", 1)
-								.queryParam("match", true)
-								.queryParam("name", botName)
-								.build())
-						.retrieve()
-						.bodyToMono(String.class).map(response -> {
-							if (response != null) {
-								ObjectMapper mapper = new ObjectMapper();
-								try {
-									JsonNode root = mapper.readTree(response);
-									if(root.path("result") != null && root.path("result").get(0) != null
-											&& !root.path("result").get(0).isEmpty()) {
-										JsonNode botNode = root.path("result").get(0);
-										return BotUtil.getBotNodeAdapterId(botNode);
-									}
-									return null;
-								} catch (JsonProcessingException jsonMappingException) {
-									return null;
-								}
-		
-							} else {
-							}
-							return null;
-						})
-						.doOnError(throwable -> log.info("Error in getting adpater: " + throwable.getMessage()))
-						.onErrorReturn(""))
-				.andWriteWith((key, signal) -> Mono.fromRunnable(
-						() -> Optional.ofNullable(signal.get()).ifPresent(value -> cache.put(key, value))))
-				.log("cache");
-	}
+//	/**
+//	 * Get Adapter id by bot name
+//	 * @param botName
+//	 * @return
+//	 */
+//	public Mono<String> getAdapterIdFromBotName(String botName) {
+//		String cacheKey = "valid-adpater-from-bot-name: " + botName;
+//		return CacheMono.lookup(key -> Mono.justOrEmpty(cache.getIfPresent(cacheKey) != null ? cache.getIfPresent(key).toString() : null)
+//					.map(Signal::next), cacheKey)
+//				.onCacheMissResume(() -> webClient.get()
+//						.uri(builder -> builder.path("admin/bot/search/internal")
+//								.queryParam("perPage", 5)
+//								.queryParam("page", 1)
+//								.queryParam("match", true)
+//								.queryParam("name", botName)
+//								.build())
+//						.retrieve()
+//						.bodyToMono(String.class).map(response -> {
+//							if (response != null) {
+//								log.info("Call getAdapterIdFromBotName : " + response);
+//								ObjectMapper mapper = new ObjectMapper();
+//								try {
+//									JsonNode root = mapper.readTree(response);
+//									if(root.path("result") != null && root.path("result").get(0) != null
+//											&& !root.path("result").get(0).isEmpty()) {
+//										JsonNode botNode = root.path("result").get(0);
+//										return BotUtil.getBotNodeAdapterId(botNode);
+//									}
+//									return null;
+//								} catch (JsonProcessingException jsonMappingException) {
+//									return null;
+//								}
+//
+//							} else {
+//							}
+//							return null;
+//						})
+//						.doOnError(throwable -> log.info("Error in getting adpater: " + throwable.getMessage()))
+//						.onErrorReturn(""))
+//				.andWriteWith((key, signal) -> Mono.fromRunnable(
+//						() -> Optional.ofNullable(signal.get()).ifPresent(value -> cache.put(key, value))))
+//				.log("cache");
+//	}
 
 	/**
 	 * Retrieve bot id from bot name (from validated bot)
@@ -227,6 +229,7 @@ public class BotService {
 							@Override
 							public String apply(String response) {
 								if (response != null) {
+									log.info("Call getBotIdFromBotName : " + response + " cache : " + cache.getIfPresent(cacheKey));
 									ObjectMapper mapper = new ObjectMapper();
 									try {
 										JsonNode root = mapper.readTree(response);
@@ -350,7 +353,7 @@ public class BotService {
 		return getAdapterByID(adapterID).map(new Function<JsonNode, Mono<JsonNode>>() {
 			@Override
 			public Mono<JsonNode> apply(JsonNode adapter) {
-				log.info("adapter: "+adapter);
+				log.info("getAdapterByID : "+adapter);
 				if(adapter != null) {
 					String vaultKey;
 					try{
@@ -464,6 +467,7 @@ public class BotService {
 								 @Override
 								 public String apply(String response) {
 									 if (response != null) {
+										 log.info("Call getFirstFormByBotID : " + response + " cache : " + cache.getIfPresent(cacheKey));
 										 ObjectMapper mapper = new ObjectMapper();
 										 try {
 											 JsonNode root = mapper.readTree(response);
@@ -505,6 +509,7 @@ public class BotService {
 								 @Override
 								 public String apply(String response) {
 									 if (response != null) {
+										 log.info("Call getBotNameByBotID : " + response + " cache : " + cache.getIfPresent(cacheKey));
 										 ObjectMapper mapper = new ObjectMapper();
 										 try {
 											 JsonNode root = mapper.readTree(response);
